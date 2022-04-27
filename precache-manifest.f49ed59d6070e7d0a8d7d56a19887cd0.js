37e3c1d35ee68ab3f3b84be8b88bc391e188a765
Podoselov <alexpodoselov1@gmail.com>
2022-04-28 01:10:36 +0300

    Updates

---
+++ precache-manifest.f49ed59d6070e7d0a8d7d56a19887cd0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e0a5cfa98ba161ccf1a",
+    "url": "/Online-Store/static/js/main.09cf8118.chunk.js"
+  },
+  {
+    "revision": "8b06dc358ea7dcaa07a6",
+    "url": "/Online-Store/static/js/runtime~main.ed7c7488.js"
+  },
+  {
+    "revision": "622074404982679ac761",
+    "url": "/Online-Store/static/js/2.bc19d576.chunk.js"
+  },
+  {
+    "revision": "d1ddcd64be455130173af9ee7582ea08",
+    "url": "/Online-Store/index.html"
+  }
+];
